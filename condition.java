582bc1fc9a43d1073d5902970c77a764d1d77192
shishirmane2003 <142569501+shishirmane2003@users.noreply.github.com>
2023-08-18 21:39:25 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,11 @@
+public class Condition{
+    public static void main(String[] arg){
+        int number ; //declaration 
+        number=29; //Initialization
+        if(number%2  != 0){
+             System.out.print(number  + "is odd number");
+        }else{
+            System.out.print(number  + "is even number");
+        }
+    }
+}   
